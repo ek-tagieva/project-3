895ce6f8f6ef63ac65ad0a184484b79f147eed37
Ekaterina Tagieva <ek.tagieva@gmail.com>
2024-02-27 00:09:48 +0300

    fix

--- tasksDOM/simpleSlider-1/script.js
+++ tasksDOM/simpleSlider-1/script.js
@@ -1,14 +1,14 @@
-// Обработчик события для кнопки "Влево"
+
 document.getElementById('prevBtn').addEventListener('click', function() {
     if (currentPosition < 0) {
-      currentPosition += 100; // Измените значение сдвига по вашему усмотрению
+      currentPosition += 100; 
       sliderContainer.style.transform = translateX(${currentPosition}px);
     }
   });
   
-  // Обработчик события для кнопки "Вправо"
+
   document.getElementById('nextBtn').addEventListener('click', function() {
-    if (currentPosition > -200) { // Измените значение сдвига по вашему усмотрению
+    if (currentPosition > -200) { 
       currentPosition -= 100;
       sliderContainer.style.transform = translateX(${currentPosition}px);
     }
